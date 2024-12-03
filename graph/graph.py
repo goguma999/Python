f2af8e90ee12d6e6d8018e6bad7da00a99db260e
goguma999 <miny9501@gmail.com>
2024-12-03 14:48:22 +0900

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,4 +1,4 @@
-##### pie graph
+##### pie 
 import matplotlib.pyplot as plt 
 
 a = emp.groupby('job')['sal'].sum().reset_index()
@@ -9,4 +9,26 @@ colors = plt.cm.Pastel1([0, 1, 2, 3, 4])  # 5개의 색상 선택
 plt.figure(figsize=(4,4))
 plt.pie(a['sumsal'], labels=a['job'], autopct='%.1f%%', colors=colors, explode=[0.2, 0.02, 0.02, 0.02, 0.02])
 plt.show()
+
+
+##### line_express
+#x_data, y_data 리스트로 준비
+import plotly.express as px
+fig = px.line(x=x_data, y=y_data)
+fig.show()
+
+
+##### line_graph_objects
+#x_data, y_data 리스트로 준비
+import plotly.graph_objects as go
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=x_data, y=y_data, mode='lines+markers', name='Sentiment'))
+fig.update_layout(
+    title="Sentiment Line Graph",
+    xaxis_title="Index",
+    yaxis_title="Sentiment",
+    template="plotly_white")
+fig.show()
+
+
 
