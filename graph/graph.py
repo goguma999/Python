37192fd921fac2d1f4cc7f7d345963b7e156d00f
goguma999 <miny9501@gmail.com>
2024-12-04 15:38:25 +0900

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,22 +1,21 @@
 
 ##### 원형 그래프 코드(matplotlib)
-import matplotlib.pyplot as plt 
+# import matplotlib.pyplot as plt 
+# a = emp.groupby('job')['sal'].sum().reset_index()
+# a.columns = ['job','sumsal']
 
-a = emp.groupby('job')['sal'].sum().reset_index()
-a.columns = ['job','sumsal']
+# colors = plt.cm.Pastel1([0, 1, 2, 3, 4])  # 5개의 색상 선택
 
-colors = plt.cm.Pastel1([0, 1, 2, 3, 4])  # 5개의 색상 선택
-
-plt.figure(figsize=(4,4))
-plt.pie(a['sumsal'], labels=a['job'], autopct='%.1f%%', colors=colors, explode=[0.2, 0.02, 0.02, 0.02, 0.02])
-plt.show()
+# plt.figure(figsize=(4,4))
+# plt.pie(a['sumsal'], labels=a['job'], autopct='%.1f%%', colors=colors, explode=[0.2, 0.02, 0.02, 0.02, 0.02])
+# plt.show()
 
 
 
 ### 원형 그래프 함수(plotly)
 #labels, values 리스트로 준비 
 import plotly.graph_objects as go 
-def plot_pie(labels, values) 
+def plot_pie(labels, values): 
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)])  
     fig.update_layout(
         title="원형 그래프 제목",
@@ -34,9 +33,9 @@ def plot_pie(labels, values)
 
 ##### 라인 그래프 코드
 #x, y 리스트로 준비
-import plotly.express as px
-fig = px.line(x=x, y=y)
-fig.show()
+# import plotly.express as px
+# fig = px.line(x=x, y=y)
+# fig.show()
 
 
 
@@ -58,7 +57,6 @@ def plot_line(x,y):
 ##### 막대 그래프 함수 
 #x, y 데이터 리스트로 준비
 import plotly.graph_objects as go 
-
 def plot_bar(x, y):
     fig = go.Figure(data=[go.Bar(x=x, y=y)])
     fig.update_layout(
