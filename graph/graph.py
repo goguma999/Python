f9595b69d6454ab271dfae654f0627ca8027c194
goguma999 <miny9501@gmail.com>
2024-12-05 17:07:08 +0900

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,5 +1,5 @@
 
-### pie1 원형 그래프 함수(plotly)
+### pie1_plotly
 #labels, values 리스트로 준비 
 import plotly.graph_objects as go 
 def plot_pie(labels, values): 
@@ -17,21 +17,21 @@ def plot_pie(labels, values):
 
 
 
-##### pie2 원형 그래프 코드(matplotlib)
-# import matplotlib.pyplot as plt 
-
-# a = emp.groupby('job')['sal'].sum().reset_index()
-# a.columns = ['job','sumsal']
-
-# colors = plt.cm.Pastel1([0, 1, 2, 3, 4])  # 5개의 색상 선택
-
-# plt.figure(figsize=(4,4))
-# plt.pie(a['sumsal'], labels=a['job'], autopct='%.1f%%', colors=colors, explode=[0.2, 0.02, 0.02, 0.02, 0.02])
-# plt.show()
-
+##### pie2_matplotlib
+import matplotlib.pyplot as plt 
+def matplotlib_pie(labels, values):
+    plt.figure(figsize=(8, 5))
+    colors = plt.cm.Pastel1([0,1,2,3,4])  # 5개의 색상 선택
+    plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
+    #plt.pie(explode = [0.2, 0.02, 0.02, 0.02, 0.02]로 조각 분리 가능)  
+    plt.title("Matplotlib Pie Chart")
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.show()
 
 
-##### line1 라인 그래프 함수
+
+
+##### line1_plotly
 #x, y 데이터 리스트로 준비 
 import plotly.graph_objects as go
 def plot_line(x,y):
@@ -46,7 +46,7 @@ def plot_line(x,y):
 
 
 
-##### line2 라인 그래프 코드
+##### 라인 그래프 코드
 #x, y 리스트로 준비
 # import plotly.express as px
 # fig = px.line(x=x, y=y)
@@ -54,7 +54,7 @@ def plot_line(x,y):
 
 
 
-##### bar1 막대 그래프 함수 
+##### bar1_plotly
 #x, y 데이터 리스트로 준비
 import plotly.graph_objects as go 
 
@@ -62,14 +62,31 @@ def plot_bar(x, y):
     fig = go.Figure(data=[go.Bar(x=x, y=y)])
     fig.update_layout(
         #title="막대 그래프 제목",
-        xaxis_title="X 축",
-        yaxis_title="Y 축",
+        xaxis_title="X",
+        yaxis_title="Y",
         template="plotly",   #다른 템플릿 ~ "plotly_dark", "ggplot2"
         font = dict(
                 family="Arial, sans-serif",
                 size=14,
                 color="RebeccaPurple" ) )
     fig.show()
+
+
+
+##### bar2_matplotlib
+import matplotlib.pyplot as plt 
+def matplotlib_bar(x, y):
+    plt.figure(figsize=(8, 5))
+    plt.bar(x, y, color='lightblue')
+    #plt.title("Matplotlib Bar Graph")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.show()
+
+
+
+
 
 
 
